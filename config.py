a228daf045cae755e3f1d2962192aa09b4378092
Jackred <jackred@tuta.io>
2020-11-16 15:11:10 +0000

    UPDATE config.py default no login

--- config.py
+++ config.py
@@ -10,12 +10,15 @@ import os
 
 class DefaultConfig:
     """ Bot Configuration """
+    HOST = "0.0.0.0"
     PORT = 3978
     APP_ID = os.environ.get("MicrosoftAppId", "")
     APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    PREFIX = "!"
 
 
 class DBConfig:
     """DB Configuration"""
     port = 27017
-    host = "localhost"
+    host = "ng_mongo"
+    db = "klabotermann"
